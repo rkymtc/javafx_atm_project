71d04c0c6b772e7357b0b76bcedb00b144a8ba21
Hamit Mizrak <hamitmizrak@gmail.com>
2025-03-18 20:07:15 +0300

    JavaFX Initial commit

---
+++ src/main/java/com/hamitmizrak/ibb_ecodation_javafx/HelloController.java
@@ -0,0 +1,14 @@
+package com.hamitmizrak.ibb_ecodation_javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
