8e0d48e71c4b369dc6f3ca7c87682aea947f0429
Hamit Mizrak <hamitmizrak@gmail.com>
2025-03-18 20:27:14 +0300

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,14 @@
 module com.hamitmizrak.ibb_ecodation_javafx {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    //requires javafx.web;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+    //requires eu.hansolo.tilesfx;
 
     opens com.hamitmizrak.ibb_ecodation_javafx to javafx.fxml;
     exports com.hamitmizrak.ibb_ecodation_javafx;
