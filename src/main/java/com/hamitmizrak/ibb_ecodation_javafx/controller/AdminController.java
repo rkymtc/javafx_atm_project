fdb8f3e3bf12e05db79372d3a5c7ec93af0d408a
Hamit Mizrak <hamitmizrak@gmail.com>
2025-03-25 20:25:07 +0300

    admin.fxml

--- src/main/java/com/hamitmizrak/ibb_ecodation_javafx/controller/AdminController.java
+++ src/main/java/com/hamitmizrak/ibb_ecodation_javafx/controller/AdminController.java
@@ -1,6 +1,16 @@
 package com.hamitmizrak.ibb_ecodation_javafx.controller;
 
+import javafx.event.ActionEvent;
+
 public class AdminController {
+    public void addUser(ActionEvent actionEvent) {
+    }
+
+    public void updateUser(ActionEvent actionEvent) {
+    }
+
+    public void deleteUser(ActionEvent actionEvent) {
+    }
 
     // Injection
 
