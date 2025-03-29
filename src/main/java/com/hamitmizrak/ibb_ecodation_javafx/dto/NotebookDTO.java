02f5fd478d3968bd3d4ffa2f4282da736d9ff8f8
Hamit Mizrak <hamitmizrak@gmail.com>
2025-03-29 15:50:23 +0300

    NotebookDTO

--- src/main/java/com/hamitmizrak/ibb_ecodation_javafx/dto/NotebookDTO.java
+++ src/main/java/com/hamitmizrak/ibb_ecodation_javafx/dto/NotebookDTO.java
@@ -1,7 +1,14 @@
 package com.hamitmizrak.ibb_ecodation_javafx.dto;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class NotebookDTO {
 
     private Long id;
@@ -11,89 +18,8 @@ public class NotebookDTO {
     private LocalDateTime updatedDate;
     private String category; // Örn: "Kişisel", "İş", "Okul"
     private boolean pinned;  // Sabitlenmiş not mu?
+    private UserDTO userDTO; //Composition
 
     // Constructorlar
-    public NotebookDTO() {
-    }
-
-    public NotebookDTO(Long id, String title, String content, LocalDateTime createdDate,
-                       LocalDateTime updatedDate, String category, boolean pinned) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.createdDate = createdDate;
-        this.updatedDate = updatedDate;
-        this.category = category;
-        this.pinned = pinned;
-    }
-
     // Getter ve Setter'lar
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(LocalDateTime updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public boolean isPinned() {
-        return pinned;
-    }
-
-    public void setPinned(boolean pinned) {
-        this.pinned = pinned;
-    }
-
-    // toString (opsiyonel)
-    @Override
-    public String toString() {
-        return "NotebookDto{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", category='" + category + '\'' +
-                ", createdDate=" + createdDate +
-                ", pinned=" + pinned +
-                '}';
-    }
 }
